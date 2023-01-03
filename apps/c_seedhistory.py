b8cce696c553fa8a89d73a455f3fd35ae7dbe82f
harmkenn <harmkenn@gmail.com>
2023-01-03 08:22:43 -0700

    sh

--- apps/c_seedhistory.py
+++ apps/c_seedhistory.py
@@ -1,9 +1,11 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
 
 def app():
     # title of the app
     st.markdown('Seed Success History Since 1985')
     df = pd.read_csv('notebooks/step04_SeedHistory.csv')
+    df.index = np.arange(1, len(df) + 1)
           
     st.dataframe(df)
