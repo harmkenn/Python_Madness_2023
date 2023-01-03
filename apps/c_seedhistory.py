a9de7cdc9cfdaab58571bc41f0dc32d83a04311a
harmkenn <harmkenn@gmail.com>
2023-01-03 08:24:49 -0700

    sh

--- apps/c_seedhistory.py
+++ apps/c_seedhistory.py
@@ -6,6 +6,6 @@ def app():
     # title of the app
     st.markdown('Seed Success History Since 1985')
     df = pd.read_csv('notebooks/step04_SeedHistory.csv')
-    df.index = np.arange(1, len(df) + 1)
+    df.index = df.index + 1
           
     st.dataframe(df)
