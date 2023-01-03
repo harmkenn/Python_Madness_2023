16604d287306c5b1c948f3b30d3f35e24b7eafe1
harmkenn <harmkenn@gmail.com>
2023-01-03 08:56:36 -0700

    SH

--- apps/c_seedhistory.py
+++ apps/c_seedhistory.py
@@ -7,5 +7,16 @@ def app():
     st.markdown('Seed Success History Since 1985')
     df = pd.read_csv('notebooks/step04_SeedHistory.csv')
     df.insert(loc= 0 , column= 'Seed', value= np.arange(1,17))
+    
+    # CSS to inject contained in a string
+    hide_dataframe_row_index = """
+            <style>
+            .row_heading.level0 {display:none}
+            .blank {display:none}
+            </style>
+            """
+
+    # Inject CSS with Markdown
+    st.markdown(hide_dataframe_row_index, unsafe_allow_html=True)
           
     st.dataframe(df, height=600)
