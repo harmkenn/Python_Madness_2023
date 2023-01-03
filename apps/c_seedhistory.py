8634edaa02b0d67dfb867ab7b3a67dcfa802196a
harmkenn <harmkenn@gmail.com>
2023-01-03 08:47:49 -0700

    SH

--- apps/c_seedhistory.py
+++ apps/c_seedhistory.py
@@ -6,6 +6,6 @@ def app():
     # title of the app
     st.markdown('Seed Success History Since 1985')
     df = pd.read_csv('notebooks/step04_SeedHistory.csv')
-    df['Seed'] = np.arange(1,17)
+    df.insert(loc= 0 , column= 'Seed', value= np.arange(1,17))
           
-    st.dataframe(df, height=5000)
+    st.dataframe(df, height=600)
